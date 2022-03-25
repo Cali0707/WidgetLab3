cc921c022a44e5ae2774746c1e6ccea9edd50ecc
Cali0707 <calumramurray@gmail.com>
2022-03-25 09:33:42 -0400

    Made working dev script

--- backend/src/controllers/sensorDataController.ts
+++ backend/src/controllers/sensorDataController.ts
@@ -4,12 +4,12 @@ import WaterData from "../models/sensorDataModel";
 
 const addSensorData = async (req: Request, res: Response): Promise<void> => {
     try {
-        console.log(req.body)
+        // console.log(req.body)
         const body = req.body as Pick<ISensorData, "deviceId" | "thermistorValue" | "photoResistorValue">
-        console.table([body.deviceId, body.photoResistorValue, body.thermistorValue])
+        // console.table([body.deviceId, body.photoResistorValue, body.thermistorValue])
 
-        const waterData: ISensorData = new WaterData({...body});
-        await waterData.save();
+        const sensorData: ISensorData = new WaterData({...body});
+        await sensorData.save();
         res.status(200).json({message: "Data saved"});
     } catch (e) {
         throw e;
@@ -18,8 +18,8 @@ const addSensorData = async (req: Request, res: Response): Promise<void> => {
 
 const getSensorData = async (req: Request, res: Response): Promise<void> => {
     try {
-        const waterDataPoints: ISensorData[] = await WaterData.find()
-        res.status(200).json({ waterDataPoints })
+        const sensorDataPoints: ISensorData[] = await WaterData.find()
+        res.status(200).json({ waterDataPoints: sensorDataPoints })
     } catch (e) {
         throw e
     }
